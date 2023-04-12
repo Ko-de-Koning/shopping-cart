78097f715d0f3e29f755fb798f224b3fa43dc224
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2023-04-12 14:42:57 +0200

    feat: prop-drilling-removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import { About } from "./pages/About";
+import Component1 from "./components/Component1";
 import { Container } from "react-bootstrap";
 import { Home } from "./pages/Home";
-import { Store } from "./pages/Store";
-import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import { Store } from "./pages/Store";
+import { useState } from "react";
 
 function App() {
-  return (  
-    <ShoppingCartProvider>
-      <Navbar/>
-      <Container className="mb-4">
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
-    </ShoppingCartProvider>
+  const [name, setName] = useState("met givename");
+  function changeName() { setName("Ding a Dong met givename");
+  }
+  return ( 
+           <ShoppingCartProvider>
+        <Navbar/>
+        <Container className="mb-4">
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/store" element={<Store />} />
+              <Route path="/about" element={<About />} />
+          </Routes>
+        </Container>
+      </ShoppingCartProvider>
   )
 }
 
